1af3e0b68e37aa4d72f048183638d1862d70d250
Eugene Min <e.v.min@icloud.com>
2015-09-29 02:45:46 +1000

    Middleware fix.

--- src/Http/Middleware/MconsoleMiddleware.php
+++ src/Http/Middleware/MconsoleMiddleware.php
@@ -18,11 +18,11 @@ class MconsoleMiddleware
     public function handle($request, Closure $next)
     {
 	    /** Redirect if already authenticated */
-	    if (!$request->is('mconsole/login') && !Auth::guest())
+	    if ($request->is('mconsole/login') && Auth::user() && Auth::user()->admin)
 	    	return redirect('/mconsole');
 	    
 	    /** Redirect to login page */
-		if (Auth::guest() || !Auth::user()->admin)
+		if ((Auth::guest() || !Auth::user()->admin) && !$request->is('mconsole/login'))
 			return redirect('/mconsole/login');
 		
         return $next($request);
