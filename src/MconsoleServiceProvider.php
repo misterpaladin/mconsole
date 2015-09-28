5b1a24dc31f3a4b4104270605301073ad580c1a2
Eugene Min <e.v.min@icloud.com>
2015-09-28 23:42:54 +1000

    Naming fix.

---
+++ src/MconsoleServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Milax\Mconsole;
+
+use Illuminate\Support\ServiceProvider;
+
+class MconsoleServiceProvider extends ServiceProvider
+{
+	public function register()
+	{
+		$this->app->bind('mconsole', function ($app) {
+			return new Mconsole;
+		});
+	}
+	
+	public function boot()
+	{
+		require __DIR__ . '/Http/routes.php';
+	}
+}
