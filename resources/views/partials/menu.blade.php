26ad1a28c1635b67cb24fb6b14e993b90d741839
Eugene Min <e.v.min@icloud.com>
2015-09-29 03:31:22 +1000

    Menu partial.

---
+++ resources/views/partials/menu.blade.php
@@ -0,0 +1,10 @@
+@foreach (Config::get('project.menu') as $menu)
+	<a href="#" class="menu-item">{{ $menu['name'] }}</a>
+	@if (isset($menu['children']))
+		<div class="menu-bundle">
+			@foreach ($menu['children'] as $child)
+			<a href="/mconsole/{{ $child['link'] }}" class="menu-item @if (Request::is($child['link'])) active @endif">{{ $child['name'] }}</a>
+			@endforeach
+		</div>
+	@endif
+@endforeach
