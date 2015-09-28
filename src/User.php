42d6908d58f13818e815beb700ebb7dfbdc2ce01
Eugene Min <e.v.min@icloud.com>
2015-09-29 00:59:31 +1000

    Models now using 'App' namespace.

--- src/User.php
+++ src/User.php
@@ -1,16 +1,39 @@
 <?php
 
-namespace Milax\Mconsole;
+namespace App;
 
-class User extends \App\User
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+
+class User extends Model implements AuthenticatableContract,
+                                    AuthorizableContract,
+                                    CanResetPasswordContract
 {
+    use Authenticatable, Authorizable, CanResetPassword;
+
     /**
-     * Extend hidden fields of basic User model
-     * 
-     * (default value: ['admin', 'password', 'remember_token'])
-     * 
+     * The database table used by the model.
+     *
      * @var string
-     * @access protected
      */
-    protected $hidden = ['admin', 'password', 'remember_token'];
+    protected $table = 'users';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'email', 'password'];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = ['password', 'remember_token'];
 }
