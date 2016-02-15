c2ce2f3965e68dfa81c50507deae272de46588b9
Eugene Min <e.v.min@icloud.com>
2016-02-15 19:39:06 +1000

    Namespaces update

--- src/Milax/Mconsole/Providers/MconsoleServiceProvider.php
+++ src/Milax/Mconsole/Providers/MconsoleServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Milax\Mconsole;
+namespace Milax\Mconsole\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -20,34 +20,45 @@ class MconsoleServiceProvider extends ServiceProvider
 	 */
 	public function boot()
 	{
-		require __DIR__ . '/Http/routes.php';
+		require __DIR__ . '/../Http/routes.php';
 		
-		$this->loadTranslationsFrom(__DIR__ . '/../../resources/lang', 'mconsole');
-		$this->loadViewsFrom(__DIR__ . '/../../resources/views', 'mconsole');
+		$this->loadTranslationsFrom(__DIR__ . '/../../../resources/lang', 'mconsole');
+		$this->loadViewsFrom(__DIR__ . '/../../../resources/views', 'mconsole');
 		
 		// Copy or merge mconsole configuration
+/*
 		if (file_exists(base_path('config/mconsole.php')))
 			$this->mergeConfigFrom(
-				__DIR__ . '/../../config/mconsole.php', 'mconsole'
+				__DIR__ . '/../../../config/mconsole.php', 'mconsole'
 			);
 		else
 			$this->publishes([
-				__DIR__ . '/../../config/mconsole.php' => base_path('config/mconsole.php'),
+				__DIR__ . '/../../../config/mconsole.php' => base_path('config/mconsole.php'),
 			], 'config');
+*/
 		
 		// Assets
 		$this->publishes([
-			__DIR__ . '/../../../public' => base_path('public/massets'),
+			__DIR__ . '/../../../../public' => base_path('public/massets'),
 		], 'assets');
 		
 		// Database migrations
 		$migrations = [];
-		$dir = __DIR__ . '/../../migrations/';
-		collect(scandir(__DIR__ . '/../../migrations'))->each(function ($file) use (&$dir, &$migrations) {
+		$dir = __DIR__ . '/../../../migrations/';
+		collect(scandir(__DIR__ . '/../../../migrations'))->each(function ($file) use (&$dir, &$migrations) {
 			if (strpos($file, '.php') !== false)
 				$migrations[$dir . $file] = base_path('database/migrations/' . $file);
 		});
 		$this->publishes($migrations, 'migrations');
+		
+		// Database seeds
+		$seeds = [];
+		$dir = __DIR__ . '/../../../seeds/';
+		collect(scandir(__DIR__ . '/../../../seeds'))->each(function ($file) use (&$dir, &$seeds) {
+			if (strpos($file, '.php') !== false)
+				$seeds[$dir . $file] = base_path('database/seeds/' . $file);
+		});
+		$this->publishes($seeds, 'seeds');
 	}
 
 	/**
@@ -58,16 +69,6 @@ class MconsoleServiceProvider extends ServiceProvider
 	public function register()
 	{
 		$this->app['router']->middleware('mconsole', 'Milax\Mconsole\Http\Middleware\MconsoleMiddleware');
-	}
-
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return [];
 	}
 
 }
