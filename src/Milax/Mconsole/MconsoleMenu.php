6adb7c38f7c9913416dad3f50dca4cc3df4708c2
Eugene Min <e.v.min@icloud.com>
2016-02-15 15:36:10 +1000

    Menu and Role models with relations

--- src/Milax/Mconsole/MconsoleMenu.php
+++ src/Milax/Mconsole/MconsoleMenu.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class MconsoleMenu extends Model
 {
-	//
+	
+	public function roles()
+	{
+		return $this->belongsToMany('App\MconsoleRole', 'mconsole_roles_menus', 'role_id', 'menu_id');
+	}
+	
 }
--- src/Milax/Mconsole/MconsoleRole.php
+++ src/Milax/Mconsole/MconsoleRole.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class MconsoleRole extends Model
 {
 	protected $fillable = ['name'];
+	
+	public function menus()
+	{
+		return $this->belongsToMany('App\MconsoleMenu', 'mconsole_roles_menus', 'role_id', 'menu_id');
+	}
+	
+	public function users()
+	{
+		return $this->hasMany('App\User');
+	}
+	
 }
