55d876ab4752a31be968e901f45199204ac112e5
Eugene Min <e.v.min@icloud.com>
2016-02-17 11:17:54 +1000

    Menu partial

--- src/resources/views/mconsole/partials/menu.blade.php
+++ src/resources/views/mconsole/partials/menu.blade.php
@@ -1,10 +1,24 @@
-@foreach ($mconsole_menu as $menu)
-	<a href="#" class="menu-item">{{ $menu->name }}</a>
-	@if (isset($menu->child))
-		<div class="menu-bundle">
-			@foreach ($menu->child as $child)
-			<a href="/mconsole/{{ $child->url }}" class="menu-item @if (Request::is($child->link)) active @endif">{{ $child->name }}</a>
-			@endforeach
-		</div>
-	@endif
-@endforeach
+<!-- BEGIN MEGA MENU -->
+<!-- DOC: Apply "hor-menu-light" class after the "hor-menu" class below to have a horizontal menu with white background -->
+<!-- DOC: Remove data-hover="dropdown" and data-close-others="true" attributes below to disable the dropdown opening on mouse hover -->
+<div class="hor-menu collapse in">
+	<ul class="nav navbar-nav">
+		@foreach ($mconsole_menu as $menu)
+			<li class="menu-dropdown classic-menu-dropdown ">
+				@if (!isset($menu->child))
+					<a href="/mconsole/{{ $menu->url }}">{{ $menu->name }}</a>
+				@else
+					<a href="#">{{ $menu->name }}</a>
+					<ul class="dropdown-menu pull-left">
+						@foreach ($menu->child as $child)
+					        <li>
+					            <a href="/mconsole/{{ $child->url }}" class="nav-link">{{ $child->name }}</a>
+					        </li>
+				        @endforeach
+				    </ul>
+				@endif
+			</li>
+		@endforeach
+	</ul>
+</div>
+<!-- END MEGA MENU -->
