18af28e33960635b3561e1934cd6b43a1f1f5b34
Eugene Min <e.v.min@icloud.com>
2016-02-17 11:18:40 +1000

    User and Page models updated date mutators

--- src/Milax/Mconsole/Models/MconsoleUser.php
+++ src/Milax/Mconsole/Models/MconsoleUser.php
@@ -27,6 +27,17 @@ class MconsoleUser extends Authenticatable
 		'password', 'remember_token',
 	];
 	
+	/**
+	 * Transform updated_at proerty.
+	 * 
+	 * @access public
+	 * @return string
+	 */
+	public function getUpdatedAttribute()
+	{
+		return $this->updated_at->format('m.d.Y');
+	}
+	
 	public function role()
 	{
 		return $this->belongsTo('Milax\Mconsole\Models\MconsoleRole');
