262230955d7140870200b6286c94d6a2e45d3c32
Eugene Min <e.v.min@icloud.com>
2016-02-21 18:16:21 +1000

    MconsoleMenu model Cacheable trait

--- src/Milax/Mconsole/Models/MconsoleMenu.php
+++ src/Milax/Mconsole/Models/MconsoleMenu.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class MconsoleMenu extends Model
 {
 	
+	use \Milax\Cacheable;
+	
 	public function roles()
 	{
 		return $this->belongsToMany('Milax\Mconsole\Models\MconsoleRole', 'mconsole_roles_menus', 'role_id', 'menu_id');
