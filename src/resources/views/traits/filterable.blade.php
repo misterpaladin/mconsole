550b6d5523c91c73f0307a646e411accc24188fc
Eugene Min <e.v.min@icloud.com>
2016-02-25 16:34:39 +1000

    Filters view for Filterable trait

---
+++ src/resources/views/traits/filterable.blade.php
@@ -0,0 +1,35 @@
+<form action="" method="GET" class="form-horizontal">
+	<div class="form-body">
+		@foreach ($filterable as $filter)
+			<div class="form-group">
+				<label class="col-md-3 control-label">{{ $filter['label'] }}</label>
+				@if ($filter['type'] == 'text')
+					<div class="col-md-4">
+						<input name="{{ $filter['key'] }}" class="form-control" placeholder="" type="text" value="{{ Request::query($filter['key']) }}">
+					</div>
+				@elseif ($filter['type'] == 'select')
+					<div class="col-md-4">
+						<select name="{{ $filter['key'] }}" class="form-control">
+							<option value="">Not selected</option>
+							@foreach ($filter['options'] as $key => $name)
+								@if (Request::query($filter['key']) == $key)
+									<option value="{{ $key }}" selected="selected">{{ $name }}</option>
+								@else
+									<option value="{{ $key }}">{{ $name }}</option>
+								@endif
+							@endforeach
+						</select>
+					</div>
+				@endif
+			</div>
+		@endforeach
+	</div>
+	<div class="form-actions">
+		<div class="row">
+			<div class="col-md-offset-3 col-md-9">
+				<button type="submit" class="btn btn-sm blue">Apply filter</button>
+				<a href="#" class="btn btn-sm">Reset filter</a>
+			</div>
+		</div>
+	</div>
+</form>
