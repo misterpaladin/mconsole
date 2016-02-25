ca4460e325c5d6c1ae73c88c0a15671e60523abc
Eugene Min <e.v.min@icloud.com>
2016-02-25 16:21:17 +1000

    Menu composer for user roles

--- src/Milax/Mconsole/Http/Composers/MenuComposer.php
+++ src/Milax/Mconsole/Http/Composers/MenuComposer.php
@@ -6,6 +6,11 @@ use Illuminate\View\View;
 
 use Milax\Mconsole\Models\MconsoleMenu;
 
+use Auth;
+
+/**
+ * Class for composing mconsole menu
+ */
 class MenuComposer
 {
 	
@@ -18,10 +23,34 @@ class MenuComposer
 	 */
 	public function compose(View $view)
 	{
+		$allowed = Auth::user()->role->menus;
+		
 		$all = MconsoleMenu::getCached();
-		$menu = $all->where('menu_id', 0);
-		$menu->each(function ($parent) use (&$all) {
-			$parent->child = $all->where('menu_id', $parent->id);
+		
+		$filtered = collect();
+		
+		$all->each(function ($menu) use (&$allowed, &$all, &$filtered) {
+			if ($menu->visible && $menu->enabled) {
+				if ((Auth::user()->role->key == 'root') || (strlen($menu->url) > 0 && $menu->menu_id == 0 && $allowed->where('id', $menu->id)->count() > 0)) {
+					$filtered->push($menu);
+				}
+				
+				if ($all->where('menu_id', $menu->id)->count() > 0) {
+					foreach ($all->where('menu_id', $menu->id) as $child) {
+						if (strlen($child->url) > 0 && $allowed->where('id', $child->id)->count() > 0) {
+							if ($filtered->where('id', $menu->id)->count() == 0)
+								$filtered->push($menu);
+							
+							$filtered->push($child);
+						}
+					}
+				}
+			}
+		});
+		
+		$menu = $filtered->where('menu_id', 0);
+		$menu->each(function ($parent) use (&$filtered) {
+			$parent->child = $filtered->where('menu_id', $parent->id);
 		});
 		
 		$view->with('mconsole_menu', $menu);
