fcd28f4c7c4b211c8355b3a3899288ac3d120cfb
Eugene Min <e.v.min@icloud.com>
2016-02-25 16:21:51 +1000

    Variables + Blade directive @variable($key)

--- src/Milax/Mconsole/Providers/MconsoleBladeExtensions.php
+++ src/Milax/Mconsole/Providers/MconsoleBladeExtensions.php
@@ -5,6 +5,9 @@ namespace Milax\Mconsole\Providers;
 use Illuminate\Support\ServiceProvider;
 
 use Milax\Mconsole\Models\MconsoleUploadPreset;
+use Milax\Mconsole\Models\Variable;
+
+use String;
 
 use Blade;
 use View;
@@ -18,14 +21,7 @@ class MconsoleBladeExtensions extends ServiceProvider
 	 */
 	public function boot()
 	{
-		Blade::directive('datetime', function($expression) {
-			return "<?php echo \Carbon\Carbon::now()->format({$expression}); ?>";
-		});
-
-		Blade::directive('upload', function ($expression) {
-			$presets = MconsoleUploadPreset::getCached();
-			return "<?php echo View::make('mconsole::mconsole.partials.upload')->with('presets', '{$presets}'); ?>";
-		});
+		//
 	}
 
 	/**
@@ -35,12 +31,23 @@ class MconsoleBladeExtensions extends ServiceProvider
 	 */
 	public function register()
 	{
-		//
-	}
-
-	protected function clean($string)
-	{
-		return str_replace(array( '(', ')' ), '', $string);
+		Blade::directive('datetime', function($expression) {
+			return "<?php echo \Carbon\Carbon::now()->format({$expression}); ?>";
+		});
+		
+		Blade::directive('upload', function ($expression) {
+			$presets = MconsoleUploadPreset::getCached();
+			return "<?php echo View::make('mconsole::partials.upload')->with('presets', '{$presets}'); ?>";
+		});
+		
+		Blade::directive('variable', function ($expression) {
+			$string = new String($expression);
+			$expression = $string->removeQuote()->removeParenthesis()->getString();
+			$variable = Variable::getCached()->where('key', $expression)->first();
+			$value = ($variable) ? $variable->value : null;
+			
+			return "<?php echo \"{$value}\"; ?>";
+		});
 	}
 
 }
