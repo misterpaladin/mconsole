63e959f5dc2f0015b1ffae50fab95cbfb8856553
Eugene Min <e.v.min@icloud.com>
2016-02-26 10:53:41 +1000

    Default mconsole config

--- src/config/mconsole.php
+++ src/config/mconsole.php
@@ -1,20 +1,7 @@
 <?php
 
 return [
-	'name' => 'New Project',
 	'menu' => [
-		[
-			'name' => 'Контент',
-			'children' => [
-				[
-					'link' => 'pages/create',
-					'name' => 'Добавить страницу'
-				],
-				[
-					'link' => 'pages',
-					'name' => 'Все страницы'
-				],
-			],
-		],
-	],
+		
+	];
 ];
