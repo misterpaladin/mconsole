73c0743af07fccaf05bae8e4b24193ae4eff64e4
vladimir-r <rudenko@milax.ru>
2016-02-26 14:15:18 +1000

    Fix forms placeholder

--- src/resources/views/forms/text.blade.php
+++ src/resources/views/forms/text.blade.php
@@ -1,4 +1,4 @@
 <div class="form-group">
 	<label>{{ $label }}</label>
-	{!! Form::text($name, null, ['placeholder' => $placeholder, 'class' => 'form-control']) !!}
+	{!! Form::text($name, null, ['placeholder' => (isset($placeholder)) ? $placeholder : null, 'class' => 'form-control']) !!}
 </div>
