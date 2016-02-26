8e6f8ccfd758f6c81f53aa71cf8df185cfd91998
Eugene Min <e.v.min@icloud.com>
2016-02-26 17:43:56 +1000

    Remove date mutators

--- src/Milax/Mconsole/Models/Page.php
+++ src/Milax/Mconsole/Models/Page.php
@@ -9,17 +9,6 @@ class Page extends Model
 	
 	protected $fillable = ['slug', 'title', 'heading', 'preview', 'text', 'description', 'hide_heading', 'fullwidth', 'system', 'enabled'];
 	
-	/**
-	 * Transform updated_at proerty.
-	 * 
-	 * @access public
-	 * @return string
-	 */
-	public function getUpdatedAttribute()
-	{
-		return $this->updated_at->format('m.d.Y');
-	}
-	
 	public function setSlugAttribute($value)
 	{
 		if (strlen($value) == 0) {
