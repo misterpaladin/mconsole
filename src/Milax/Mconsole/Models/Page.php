5bd1d201257aea5a80f8a86edd0abc4e3b235c9c
Eugene Min <e.v.min@icloud.com>
2016-03-03 12:52:30 +1000

    Pages section multi language support #15

--- src/Milax/Mconsole/Models/Page.php
+++ src/Milax/Mconsole/Models/Page.php
@@ -8,6 +8,15 @@ class Page extends Model
 {
     protected $fillable = ['slug', 'title', 'heading', 'preview', 'text', 'description', 'hide_heading', 'fullwidth', 'system', 'enabled'];
     
+    protected $casts = [
+        'heading' => 'array',
+        'preview' => 'array',
+        'text' => 'array',
+        'title' => 'array',
+        'description' => 'array',
+        'links' => 'array',
+    ];
+    
     public function setSlugAttribute($value)
     {
         if (strlen($value) == 0) {
