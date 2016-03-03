f332ca39557f92e529613c572be3385f4fc9f19b
Eugene Min <e.v.min@icloud.com>
2016-03-03 14:24:41 +1000

    Return $this in HasView trait for method chaining

--- src/Milax/Mconsole/Traits/HasView.php
+++ src/Milax/Mconsole/Traits/HasView.php
@@ -11,15 +11,18 @@ trait HasView
         view()->composer('mconsole::app', function ($view) use (&$value) {
             $view->with('pageTitle', $value  . ' | Mconsole');
         });
+        return $this;
     }
     
     protected function setCaption($value)
     {
         View::share('pageCaption', $value);
+        return $this;
     }
     
     protected function setSubcaption($value)
     {
         View::share('pageSubcaption', $value);
+        return $this;
     }
 }
