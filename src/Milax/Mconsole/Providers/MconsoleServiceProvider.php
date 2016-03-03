e75e0a4ca122370731964fbded4bc948cc6c3397
Eugene Min <e.v.min@icloud.com>
2016-03-03 15:08:25 +1000

    HasView trait removed

--- src/Milax/Mconsole/Providers/MconsoleServiceProvider.php
+++ src/Milax/Mconsole/Providers/MconsoleServiceProvider.php
@@ -55,7 +55,6 @@ class MconsoleServiceProvider extends ServiceProvider
                 'Paginatable' => \Milax\Mconsole\Traits\Paginatable::class,
                 'Filterable' => \Milax\Mconsole\Traits\Filterable::class,
                 'HasQueryTraits' => \Milax\Mconsole\Traits\HasQueryTraits::class,
-                'HasView' => \Milax\Mconsole\Traits\HasView::class,
             ],
             
             // Interface to Implementation bindings
