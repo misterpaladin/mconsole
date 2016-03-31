cb1eae170525a799457e9a80f900eae40533e2f6
Eugene Min <e.v.min@icloud.com>
2016-04-01 01:57:26 +1000

    TG-12 #ready-for-test
    Dynamic relationships

--- src/Milax/Mconsole/Models/MconsoleModel.php
+++ src/Milax/Mconsole/Models/MconsoleModel.php
@@ -2,31 +2,11 @@
 
 namespace Milax\Mconsole\Models;
 
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
 use Config;
 
-class MconsoleUser extends Authenticatable
+class MconsoleModel extends Model
 {
-    protected $table = 'users';
-    
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password', 'lang',
-    ];
-
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-    
     /**
      * Magic method to load relationships
      * 
