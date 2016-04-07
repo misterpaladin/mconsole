a5981d9d6df72451f23f10b004544b2e24eca34d
Eugene Min <e.v.min@icloud.com>
2016-04-08 01:41:02 +1100

    TG-33 #closed
    Relationships between MconsoleUploadPreset and Image, migration with additional columns

---
+++ src/migrations/2016_04_07_143322_modify_images_table_add_additional_columns.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ModifyImagesTableAddAdditionalColumns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('images', function (Blueprint $table) {
+            $table->integer('preset_id')->after('id');
+            $table->text('copies')->after('filename');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('images', function (Blueprint $table) {
+            $table->dropColumn('preset_id');
+            $table->dropColumn('copies');
+        });
+    }
+}
