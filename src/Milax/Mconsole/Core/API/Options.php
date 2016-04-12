b72be3e1786d638ef1677f7a7a57d852f26a72e0
Eugene Min <e.v.min@icloud.com>
2016-04-12 19:24:20 +1100

    TG-104 #closed

--- src/Milax/Mconsole/Core/API/Options.php
+++ src/Milax/Mconsole/Core/API/Options.php
@@ -17,4 +17,45 @@ class Options extends ModelAPI
         $model = $this->model;
         return $model::getByKey($key);
     }
+    
+    /**
+     * Create or update options
+     *
+     * @param array $options [Options array]
+     * @return void
+     */
+    public function install($options)
+    {
+        $model = $this->model;
+        foreach ($options as $option) {
+            if ($model::where('key', $option['key'])->count() == 0) {
+                $model::create($option);
+            } else {
+                unset($option['value']);
+                
+                if (isset($option['rules'])) {
+                    $option['rules'] = json_encode($option['rules']);
+                }
+                
+                if (isset($option['options'])) {
+                    $option['options'] = json_encode($option['options']);
+                }
+                $model::where('key', $option['key'])->update($option);
+            }
+        }
+    }
+    
+    /**
+     * Remove options from database
+     *
+     * @param array $options [Options array]
+     * @return void
+     */
+    public function uninstall($options)
+    {
+        $model = $this->model;
+        foreach ($options as $option) {
+            $model::where('key', $option['key'])->delete();
+        }
+    }
 }
