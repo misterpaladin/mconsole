1a86de365a9df79c87b9a695b326742dc9e21028
Eugene Min <e.v.min@icloud.com>
2016-04-15 16:02:01 +1100

    Fix color picker

--- src/resources/views/forms/colorpicker.blade.php
+++ src/resources/views/forms/colorpicker.blade.php
@@ -1,4 +1,4 @@
 <div class="form-group">
 	<label>{{ $label }}</label>
-	{!! Form::hidden($name, (isset($value)) ? $value : '#0088cc', ['class' => 'form-control color-picker']) !!}
+	{!! Form::hidden($name, null, ['class' => 'form-control color-picker']) !!}
 </div>
