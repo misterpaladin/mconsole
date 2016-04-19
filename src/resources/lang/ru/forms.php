25eab892b355a1bd08a054d76d458b12ae80ed16
Eugene Min <e.min@milax.com>
2016-04-19 22:29:54 +1100

    Translations for forms

--- src/resources/lang/ru/forms.php
+++ src/resources/lang/ru/forms.php
@@ -1,10 +1,27 @@
 <?php
 
-
 return [
     'submit' => 'Сохранить',
     'tags' => [
         'label' => 'Теги',
         'placeholder' => 'Выбрать теги',
+    ],
+    'tabs' => [
+        'main' => 'Основное',
+        'settings' => 'Доп. параметры',
+    ],
+    'options' => [
+        'state' => [
+            'enabled' => 'Включено',
+            'disabled' => 'Отключено',
+        ],
+        'yesno' => [
+            'enabled' => 'Да',
+            'disabled' => 'Нет',
+        ],
+        'show' => [
+            'enabled' => 'Отображать',
+            'disabled' => 'Не отображать',
+        ],
     ],
 ];
