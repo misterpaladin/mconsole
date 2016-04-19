7a62981cd24d83584e91dd543db1b604cdc3b960
Eugene Min <e.min@milax.com>
2016-04-19 20:47:11 +1100

    Check if defined for unit tests

--- src/Milax/Mconsole/Core/defines.php
+++ src/Milax/Mconsole/Core/defines.php
@@ -1,11 +1,19 @@
 <?php
 
-// Upload types
-define('MX_UPLOAD_TYPE_IMAGE', 'image');
-define('MX_UPLOAD_TYPE_DOCUMENT', 'document');
-define('MX_UPLOAD_TYPE_AUDIO', 'audio');
-define('MX_UPLOAD_TYPE_VIDEO', 'video');
+$defines = [
+    // Upload types
+    'MX_UPLOAD_TYPE_IMAGE' => 'image',
+    'MX_UPLOAD_TYPE_DOCUMENT' => 'document',
+    'MX_UPLOAD_TYPE_AUDIO' => 'audio',
+    'MX_UPLOAD_TYPE_VIDEO' => 'video',
+    
+    // modules
+    'MODULESEARCH' => 'Mconsole',
+    'BOOTSTRAPFILE' => 'bootstrap.php',
+];
 
-// modules
-define('MODULESEARCH', 'Mconsole');
-define('BOOTSTRAPFILE', 'bootstrap.php');
+foreach ($defines as $key => $value) {
+    if (!defined($key)) {
+        define($key, $value);
+    }
+}
