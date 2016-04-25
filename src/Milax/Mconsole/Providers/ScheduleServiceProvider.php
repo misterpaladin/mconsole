bcd280621e00db8b5f2bf6288fbc9e3079df606e
Eugene Min <e.min@milax.com>
2016-04-25 17:20:03 +1100

    TG-135
    Added schedule to run database queue

---
+++ src/Milax/Mconsole/Providers/ScheduleServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Milax\Mconsole\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Console\Scheduling\Schedule;
+
+class ScheduleServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->app->booted(function () {
+            $schedule = $this->app->make(Schedule::class);
+            $schedule->command('queue:work database')->everyMinute();
+        });
+    }
+    
+    public function register()
+    {
+        //
+    }
+}
