bee52f0e4520617d104537b3e69f27c1081b4c0c
Eugene Min <e.min@milax.com>
2016-04-26 11:04:52 +1100

    TG-142 #closed

---
+++ src/migrations/2016_04_25_130820_create_linkables_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLinkablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('linkables', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('link_id');
+            $table->integer('linkable_id');
+            $table->string('linkable_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('linkables');
+    }
+}
