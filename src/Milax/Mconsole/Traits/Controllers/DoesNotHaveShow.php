06a64c4e1a4f47e03a906680b49c0b699495d389
vladimir-r <rudenko@milax.ru>
2016-04-26 15:48:39 +1100

    DoesNotHaveShow trait comments

--- src/Milax/Mconsole/Traits/Controllers/DoesNotHaveShow.php
+++ src/Milax/Mconsole/Traits/Controllers/DoesNotHaveShow.php
@@ -4,6 +4,12 @@ namespace Milax\Mconsole\Traits\Controllers;
 
 trait DoesNotHaveShow
 {
+    /**
+     * Provides the show method for resource controllers
+     * 
+     * @param  int $id [Instance id]
+     * @return Redirector
+     */
     public function show($id) {
         $route = str_replace('show', 'index', \Request::route()->getName());
         return redirect()->route($route);
