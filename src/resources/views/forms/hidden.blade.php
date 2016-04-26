fb03655ff93be3204518559e164bead0b36b7485
Eugene Min <e.min@milax.com>
2016-04-26 11:31:56 +1100

    Fix hidden form partial value

--- src/resources/views/forms/hidden.blade.php
+++ src/resources/views/forms/hidden.blade.php
@@ -1,3 +1,3 @@
 <div class="form-group">
-	{!! Form::hidden($name, $value ? $value : null, ['placeholder' => (isset($placeholder)) ? $placeholder : null, 'class' => (isset($class)) ? $class : null]) !!}
+	{!! Form::hidden($name, isset($value) ? $value : null, ['placeholder' => (isset($placeholder)) ? $placeholder : null, 'class' => (isset($class)) ? $class : null]) !!}
 </div>
