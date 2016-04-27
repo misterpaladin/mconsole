9d3dbef8c175d6391ae24853423e21873c51e41c
Eugene Min <e.min@milax.com>
2016-04-28 00:27:07 +1100

    Fix CascadeDelete trait

--- src/Milax/Mconsole/Traits/Models/CascadeDelete.php
+++ src/Milax/Mconsole/Traits/Models/CascadeDelete.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace Milax\Mconsole\Traits\Models;
 
@@ -7,13 +7,13 @@ trait CascadeDelete
     /**
      * Register CascadeDelete callback functions
      */
-    public function __construct()
+    public function __construct($attributes = [])
     {
-        parent::__construct();
+        parent::__construct($attributes);
         $methods = get_class_methods(__CLASS__);
-        $methods = array_walk($methods, function($method) {
+        $methods = array_walk($methods, function ($method) {
             if (starts_with($method, 'cascadeDelete')) {
-                self::deleting(function($object) use ($method) {
+                self::deleting(function ($object) use ($method) {
                     $object->$method();
                 });
             }
