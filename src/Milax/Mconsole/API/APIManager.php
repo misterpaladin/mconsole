53831edaf51d9e9535937e9ada1bca49410f98d6
Eugene Min <e.min@milax.com>
2016-04-30 17:38:16 +1100

    APIManager support dot syntax namespaces

--- src/Milax/Mconsole/API/APIManager.php
+++ src/Milax/Mconsole/API/APIManager.php
@@ -18,10 +18,25 @@ class APIManager
      */
     public function register($namespace, $instance)
     {
-        if (property_exists($this, $namespace)) {
-            throw new APINamespaceExistsException(sprintf('The namespace "%s" is already registered in API, unable to register', $namespace));
+        if (str_contains($namespace, '.')) {
+            $namespaces = explode('.', $namespace);
+            $namespace = array_pull($namespaces, 0);
+            $namespaces = implode('.', $namespaces);
+            
+            if (!property_exists($this, $namespace)) {
+                $this->$namespace = [];
+            }
+            
+            if (array_get($this->$namespace, $namespaces)) {
+                throw new APINamespaceExistsException(sprintf('The namespace "%s" is already registered in API, unable to register', $namespace));
+            }
+            
+            array_set($this->$namespace, $namespaces, $instance);
+        } else {
+            if (property_exists($this, $namespace)) {
+                throw new APINamespaceExistsException(sprintf('The namespace "%s" is already registered in API, unable to register', $namespace));
+            }
+            $this->$namespace = $instance;
         }
-        
-        $this->$namespace = $instance;
     }
 }
