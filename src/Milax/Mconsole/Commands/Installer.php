ddd4849740fbd3ae8fcaaebfc5d4f57417cab1a4
Eugene Min <e.min@milax.com>
2016-05-09 01:39:02 +1100

    Create first user with app locale lang #63

--- src/Milax/Mconsole/Commands/Installer.php
+++ src/Milax/Mconsole/Commands/Installer.php
@@ -4,6 +4,7 @@ namespace Milax\Mconsole\Commands;
 
 use DB;
 use File;
+use App;
 use Illuminate\Console\Command;
 use Milax\Mconsole\Seeds;
 use Carbon\Carbon;
@@ -134,6 +135,7 @@ class Installer extends Command
                     'role_id' => 1,
                     'name' => $name,
                     'email' => $email,
+                    'lang' => App::getLocale(),
                     'password' => bcrypt($pass),
                     'created_at' => Carbon::now(),
                     'updated_at' => Carbon::now(),
