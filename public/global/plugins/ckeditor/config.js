592cf11573f1246a60dd9971e74b734bae243994
Eugene Min <e.min@milax.com>
2016-05-09 13:49:52 +1100

    Regroup CKEditor config #76

--- public/global/plugins/ckeditor/config.js
+++ public/global/plugins/ckeditor/config.js
@@ -7,19 +7,17 @@ CKEDITOR.editorConfig = function( config ) {
     config.allowedContent = true;
 	config.uiColor = 'FFFFFF';
 	config.colorButton_colors = '00923E,FFFFFF,FFFFFF';
-	config.removeButtons = 'Image,NewPage,Preview,Print,Find,Replace,SelectAll,Underline,Subscript,Outdent,Indent,Blockquote,CreateDiv,Cut,Copy,Paste,Save,Anchor,Smiley,Flash,PageBreak,Iframe,Language,JustifyLeft,JustifyRight,JustifyCenter,JustifyBlock,SpellChecker,Scayt,Font';
+	config.removeButtons = 'Image,NewPage,Preview,Print,Find,Replace,SelectAll,Underline,Subscript,Outdent,Indent,Blockquote,CreateDiv,Cut,Copy,Paste,Save,Anchor,Smiley,Flash,PageBreak,Iframe,Language,JustifyLeft,JustifyRight,JustifyCenter,JustifyBlock,SpellChecker,Scayt,Font,ShowBlocks,FontSize';
 	config.toolbarGroups = [
 	    { name: 'document',    groups: [ 'mode' ] },
-	    { name: 'clipboard',   groups: [ 'PasteText', 'undo' ] },
 	    { name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
 	    { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
 	    { name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align',] },
 	    { name: 'links' },
 	    { name: 'insert' },
+	    { name: 'clipboard',   groups: [ 'PasteText', 'undo' ] },
 	    '/',
 	    { name: 'styles' },
-	    { name: 'colors' },
 	    { name: 'tools' },
-	    { name: 'others' },
 	];
 };
