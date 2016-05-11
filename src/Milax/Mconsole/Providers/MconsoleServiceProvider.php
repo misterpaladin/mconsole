7d2cebca1e0a5d4266dad2bc22014a54e90ace80
Eugene Min <e.min@milax.com>
2016-05-11 21:01:36 +1100

    Release 0.4.9

--- src/Milax/Mconsole/Providers/MconsoleServiceProvider.php
+++ src/Milax/Mconsole/Providers/MconsoleServiceProvider.php
@@ -123,7 +123,7 @@ class MconsoleServiceProvider extends ServiceProvider
         
         // Run one time setup
         app('API')->modules->scan();
-        app('API')->info->setAppVersion('0.4.8');
+        app('API')->info->setAppVersion('0.4.9');
         
         if (env('APP_ENV') == 'local') {
             app('API')->translations->load();
