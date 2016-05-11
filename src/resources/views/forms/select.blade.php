82e24a4b91b9d98703f54b437fdcf7c295f6b62f
Eugene Min <e.min@milax.com>
2016-05-11 15:15:01 +1100

    Fix select for boolean values in postgres

--- src/resources/views/forms/select.blade.php
+++ src/resources/views/forms/select.blade.php
@@ -1,11 +1,11 @@
 <div class="form-group">
 	<label>{{ $label }}</label>
     @if (isset($options) && is_array($options))
-        {!! Form::select($name, $options, (isset($value)) ? $value : null, ['class' => 'form-control']) !!}
+        {!! Form::select($name, $options, (isset($value)) ? $value : is_bool(Form::getValueAttribute($name)) ? (int) Form::getValueAttribute($name) : null, ['class' => 'form-control']) !!}
     @elseif (isset($type))
         {!! Form::select($name, [
-            TRUE => trans(sprintf('mconsole::forms.options.%s.enabled', $type)),
-            FALSE => trans(sprintf('mconsole::forms.options.%s.disabled', $type)),
-        ], (isset($value)) ? $value : null, ['class' => 'form-control']) !!}
+            '1' => trans(sprintf('mconsole::forms.options.%s.enabled', $type)),
+            '0' => trans(sprintf('mconsole::forms.options.%s.disabled', $type)),
+        ], (isset($value)) ? $value : is_bool(Form::getValueAttribute($name)) ? (int) Form::getValueAttribute($name) : 0, ['class' => 'form-control']) !!}
     @endif
 </div>
