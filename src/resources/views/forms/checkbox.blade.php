d74a667e1b582b8bdb51b23a344fc015ee38d3d8
Eugene Min <e.min@milax.com>
2016-05-11 18:15:26 +1100

    Added checkbox forms

--- src/resources/views/forms/checkbox.blade.php
+++ src/resources/views/forms/checkbox.blade.php
@@ -1,3 +1,8 @@
-<div class="form-group checkbox-inline">
-	<label>{!! Form::checkbox($name, 1, ($value == '1')) !!} {{ $label }}</label>
+<div class="form-group">
+    @if (!is_null(Form::getValueAttribute($name)) && is_bool(Form::getValueAttribute($name)))
+	       <input type="hidden" name="{{ $name }}" value="{{ (int) !Form::getValueAttribute($name) }}" />
+    @endif
+    <div class="checkbox-list">
+        <label class="checkbox">{!! Form::checkbox($name, 1, (isset($value)) ? $value : is_bool(Form::getValueAttribute($name)) ? (int) Form::getValueAttribute($name) : null) !!} {{ $label }}</label>
+    </div>
 </div>
---
+++ src/resources/views/forms/checkboxes.blade.php
@@ -0,0 +1,12 @@
+@foreach ($items as $checkbox)
+    @if (!is_null(Form::getValueAttribute($checkbox['name'])) && is_bool(Form::getValueAttribute($checkbox['name'])))
+           <input type="hidden" name="{{ $checkbox['name'] }}" value="{{ (int) !Form::getValueAttribute($checkbox['name']) }}" />
+    @endif
+@endforeach
+<div class="form-group">
+    <div class="checkbox-list">
+        @foreach ($items as $checkbox)
+            <label class="checkbox-inline">{!! Form::checkbox($checkbox['name'], 1, (isset($checkbox['value'])) ? $checkbox['value'] : is_bool(Form::getValueAttribute($checkbox['name'])) ? (int) Form::getValueAttribute($checkbox['name']) : null) !!} {{ $checkbox['label'] }}</label>
+        @endforeach
+    </div>
+</div>
