1f736809c1d6efcee0afa61e9cd0103809f45dea
Eugene Min <e.min@milax.com>
2016-05-18 12:48:39 +1100

    Fix select view

--- src/resources/views/forms/select.blade.php
+++ src/resources/views/forms/select.blade.php
@@ -1,11 +1,11 @@
 <div class="form-group">
 	<label>{{ $label }}</label>
     @if (isset($options) && is_array($options))
-        {!! Form::select($name, $options, (isset($value)) ? $value : is_bool(Form::getValueAttribute($name)) ? (int) Form::getValueAttribute($name) : null, ['class' => 'form-control']) !!}
+        {!! Form::select($name, $options, (isset($value)) ? $value : is_bool(Form::getValueAttribute($name)) ? (int) Form::getValueAttribute($name) : null, ['class' => sprintf('form-control %s', isset($class) ? $class : null)]) !!}
     @elseif (isset($type))
         {!! Form::select($name, [
             '1' => trans(sprintf('mconsole::forms.options.%s.enabled', $type)),
             '0' => trans(sprintf('mconsole::forms.options.%s.disabled', $type)),
-        ], (isset($value)) ? $value : is_bool(Form::getValueAttribute($name)) ? (int) Form::getValueAttribute($name) : null, ['class' => 'form-control']) !!}
+        ], (isset($value)) ? $value : is_bool(Form::getValueAttribute($name)) ? (int) Form::getValueAttribute($name) : null, ['class' => sprintf('form-control %s', isset($class) ? $class : null)]) !!}
     @endif
 </div>
