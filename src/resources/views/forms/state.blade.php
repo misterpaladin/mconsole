4c7bc7083a1183e5e99d90365752d22240c2db86
Eugene Min <e.min@milax.com>
2016-05-18 12:48:32 +1100

    State as checkbox

--- src/resources/views/forms/state.blade.php
+++ src/resources/views/forms/state.blade.php
@@ -1,14 +1,5 @@
-@if (isset($item))
-    @include('mconsole::forms.select', [
-        'label' => trans('mconsole::settings.options.enabled'),
-        'options' => ['1' => trans('mconsole::settings.options.on'), '0' => trans('mconsole::settings.options.off')],
-        'name' => 'enabled',
-        'value' => ($item->enabled || $item->enabled == 1) ? 1 : 0,
-    ])
-@else
-    @include('mconsole::forms.select', [
-        'label' => trans('mconsole::settings.options.enabled'),
-        'options' => ['1' => trans('mconsole::settings.options.on'), '0' => trans('mconsole::settings.options.off')],
-        'name' => 'enabled',
-    ])
-@endif
+@include('mconsole::forms.checkbox', [
+    'label' => trans('mconsole::settings.options.on'),
+    'name' => 'enabled',
+    'checked' => true,
+])
