593317870bd6acccaf4231ecd36289aac24829e9
Eugene Min <e.min@milax.com>
2016-06-11 13:07:24 +1100

    Bind repositories

--- src/Milax/Mconsole/Providers/RepositoriesServiceProvider.php
+++ src/Milax/Mconsole/Providers/RepositoriesServiceProvider.php
@@ -16,94 +16,16 @@ class RepositoriesServiceProvider extends ServiceProvider
     
     public function register()
     {
-        $repositories = [
-            [
-                'when' => '\Milax\Mconsole\Http\Controllers\MenusController',
-                'needs' => '\Milax\Mconsole\Contracts\Repository',
-                'give' => function () {
-                    return new \Milax\Mconsole\Repositories\MenusRepository(\Milax\Mconsole\Models\Menu::class);
-                },
-                'bind' => 'menus',
-            ],
-            [
-                'when' => '\Milax\Mconsole\Http\Controllers\PresetsController',
-                'needs' => '\Milax\Mconsole\Contracts\Repository',
-                'give' => function () {
-                    return new \Milax\Mconsole\Repositories\PresetsRepository(\Milax\Mconsole\Models\MconsoleUploadPreset::class);
-                },
-                'bind' => 'presets',
-            ],
-            [
-                'when' => '\Milax\Mconsole\Http\Controllers\RolesController',
-                'needs' => '\Milax\Mconsole\Contracts\Repository',
-                'give' => function () {
-                    return new \Milax\Mconsole\Repositories\RolesRepository(\Milax\Mconsole\Models\MconsoleRole::class);
-                },
-                'bind' => 'roles',
-            ],
-            [
-                'when' => '\Milax\Mconsole\Http\Controllers\TagsController',
-                'needs' => '\Milax\Mconsole\Contracts\Repository',
-                'give' => function () {
-                    return new \Milax\Mconsole\Repositories\TagsRepository(\Milax\Mconsole\Models\Tag::class);
-                },
-                'bind' => 'tags',
-            ],
-            [
-                'when' => '\Milax\Mconsole\Http\Controllers\UploadsController',
-                'needs' => '\Milax\Mconsole\Contracts\Repository',
-                'give' => function () {
-                    return new \Milax\Mconsole\Repositories\UploadsRepository(\Milax\Mconsole\Models\Upload::class);
-                },
-                'bind' => 'uploads',
-            ],
-            [
-                'when' => '\Milax\Mconsole\Http\Controllers\UsersController',
-                'needs' => '\Milax\Mconsole\Contracts\Repository',
-                'give' => function () {
-                    return new \Milax\Mconsole\Repositories\UsersRepository(\App\User::class);
-                },
-                'bind' => 'users',
-            ],
-            [
-                'when' => '\Milax\Mconsole\Http\Controllers\SettingsController',
-                'needs' => '\Milax\Mconsole\Contracts\Repository',
-                'give' => function () {
-                    return new \Milax\Mconsole\Repositories\SettingsRepository(\Milax\Mconsole\Models\MconsoleOption::class);
-                },
-                'bind' => 'settings',
-            ],
-            [
-                'when' => '\Milax\Mconsole\Http\Controllers\VariablesController',
-                'needs' => '\Milax\Mconsole\Contracts\Repository',
-                'give' => function () {
-                    return new \Milax\Mconsole\Repositories\VariablesRepository(\Milax\Mconsole\Models\Variable::class);
-                },
-                'bind' => 'variables',
-            ],
-            [
-                'when' => '\Milax\Mconsole\Http\Controllers\ModulesController',
-                'needs' => '\Milax\Mconsole\Contracts\Repository',
-                'give' => function () {
-                    return new \Milax\Mconsole\Repositories\ModulesRepository(\Milax\Mconsole\Models\MconsoleModule::class);
-                },
-                'bind' => 'modules',
-            ],
-            [
-                'when' => '\Milax\Mconsole\Http\Controllers\LanguagesController',
-                'needs' => '\Milax\Mconsole\Contracts\Repository',
-                'give' => function () {
-                    return new \Milax\Mconsole\Repositories\LanguagesRepository(\Milax\Mconsole\Models\Language::class);
-                },
-                'bind' => 'languages',
-            ],
-        ];
-        
-        // Repositories contextual binding
-        foreach ($repositories as $repository) {
-            app('API')->repositories->register($repository['bind'], $repository['give']());
-            $this->app->when($repository['when'])->needs($repository['needs'])->give($repository['give']);
-        }
-        app('API')->repositories->register('links', new \Milax\Mconsole\Repositories\LinksRepository(\Milax\Mconsole\Models\Link::class));
+        $this->app->bind('Milax\Mconsole\Contracts\Repositories\LanguagesRepository', 'Milax\Mconsole\Repositories\LanguagesRepository');
+        $this->app->bind('Milax\Mconsole\Contracts\Repositories\LinksRepository', 'Milax\Mconsole\Repositories\LinksRepository');
+        $this->app->bind('Milax\Mconsole\Contracts\Repositories\MenusRepository', 'Milax\Mconsole\Repositories\MenusRepository');
+        $this->app->bind('Milax\Mconsole\Contracts\Repositories\ModulesRepository', 'Milax\Mconsole\Repositories\ModulesRepository');
+        $this->app->bind('Milax\Mconsole\Contracts\Repositories\PresetsRepository', 'Milax\Mconsole\Repositories\PresetsRepository');
+        $this->app->bind('Milax\Mconsole\Contracts\Repositories\RolesRepository', 'Milax\Mconsole\Repositories\RolesRepository');
+        $this->app->bind('Milax\Mconsole\Contracts\Repositories\TagsRepository', 'Milax\Mconsole\Repositories\TagsRepository');
+        $this->app->bind('Milax\Mconsole\Contracts\Repositories\UploadsRepository', 'Milax\Mconsole\Repositories\UploadsRepository');
+        $this->app->bind('Milax\Mconsole\Contracts\Repositories\UsersRepository', 'Milax\Mconsole\Repositories\UsersRepository');
+        $this->app->bind('Milax\Mconsole\Contracts\Repositories\SettingsRepository', 'Milax\Mconsole\Repositories\SettingsRepository');
+        $this->app->bind('Milax\Mconsole\Contracts\Repositories\VariablesRepository', 'Milax\Mconsole\Repositories\VariablesRepository');
     }
 }
