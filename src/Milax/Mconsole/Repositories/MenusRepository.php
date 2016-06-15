b45f3ae6d23027a8be2d73aeb81a61448800da65
Eugene Min <e.min@milax.com>
2016-06-15 19:19:07 +1100

    Fix menus repository, closes #104

--- src/Milax/Mconsole/Repositories/MenusRepository.php
+++ src/Milax/Mconsole/Repositories/MenusRepository.php
@@ -5,7 +5,7 @@ namespace Milax\Mconsole\Repositories;
 use Milax\Mconsole\Repositories\EloquentRepository;
 use Milax\Mconsole\Contracts\Repositories\MenusRepository as Repository;
 
-class MenusRepository extends EloquentRepository implements MenusRepository
+class MenusRepository extends EloquentRepository implements Repository
 {
     public $model = \Milax\Mconsole\Models\Menu::class;
 }
