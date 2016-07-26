fd996a35c6f960c5a8a13fe2ffca86b3f5f2bff2
Eugene Min <e.min@milax.com>
2016-07-26 20:20:22 +1100

    Show only enabled settings in settings page

--- src/Milax/Mconsole/Contracts/Repositories/SettingsRepository.php
+++ src/Milax/Mconsole/Contracts/Repositories/SettingsRepository.php
@@ -4,4 +4,11 @@ namespace Milax\Mconsole\Contracts\Repositories;
 
 interface SettingsRepository
 {
+    /**
+     * Get settings
+     * 
+     * @param  bool $withDisabled [Get disabled options]
+     * @return Illuminate\Database\Collection
+     */
+    public function get($withDisabled = false);
 }
--- src/Milax/Mconsole/Repositories/SettingsRepository.php
+++ src/Milax/Mconsole/Repositories/SettingsRepository.php
@@ -8,4 +8,17 @@ use Milax\Mconsole\Contracts\Repositories\SettingsRepository as Repository;
 class SettingsRepository extends EloquentRepository implements Repository
 {
     public $model = \Milax\Mconsole\Models\MconsoleOption::class;
+    
+    public function get($withDisabled = false)
+    {
+        $model = $this->model;
+        
+        if ($withDisabled) {
+            return $model::get();
+        }
+        
+        return $model::where('enabled', true)->get();
+        
+    }
+    
 }
