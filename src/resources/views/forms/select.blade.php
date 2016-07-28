8f2a13c5c640e92f407ed5427d179aebefc3c8e6
Min De Sik <e.min@milax.com>
2016-07-29 00:59:56 +1100

    Fix select form component values

--- src/resources/views/forms/select.blade.php
+++ src/resources/views/forms/select.blade.php
@@ -1,12 +1,12 @@
 <div class="form-group">
 	<label>{{ $label }}</label>
+    <?php if (isset($value)): $val = $value; elseif (is_bool(Form::getValueAttribute($name))): $val = (int) Form::getValueAttribute($name); else: $val = null; endif; ?>
     @if (isset($options) && is_array($options))
-        <?php if (isset($value)): $val = $value; elseif (is_bool(Form::getValueAttribute($name))): $val = (int) Form::getValueAttribute($name); else: $val = null; endif; ?>
         {!! Form::select($name, $options, $val, ['class' => sprintf('form-control %s', isset($class) ? $class : null)]) !!}
     @elseif (isset($type))
         {!! Form::select($name, [
             '1' => trans(sprintf('mconsole::forms.options.%s.enabled', $type)),
             '0' => trans(sprintf('mconsole::forms.options.%s.disabled', $type)),
-        ], (isset($value)) ? $value : is_bool(Form::getValueAttribute($name)) ? (int) Form::getValueAttribute($name) : null, ['class' => sprintf('form-control %s', isset($class) ? $class : null)]) !!}
+        ], $val, ['class' => sprintf('form-control %s', isset($class) ? $class : null)]) !!}
     @endif
 </div>
