4449a1e45f444ced28e52e2dca8885df2a64ae7d
vladimir-r <rudenko@milax.ru>
2016-08-02 16:03:03 +1100

    Add method getByKey for VariablesRepository

--- src/Milax/Mconsole/Contracts/Repositories/VariablesRepository.php
+++ src/Milax/Mconsole/Contracts/Repositories/VariablesRepository.php
@@ -4,4 +4,11 @@ namespace Milax\Mconsole\Contracts\Repositories;
 
 interface VariablesRepository
 {
+    /**
+     * Get variable by key
+     * 
+     * @param  string $key
+     * @return string
+     */
+    public function getByKey($key);
 }
