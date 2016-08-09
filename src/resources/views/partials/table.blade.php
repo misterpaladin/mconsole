10e85bbccb26f31e013f7c336406d94b7e900189
Min De Sik <e.min@milax.com>
2016-08-09 14:08:26 +1100

    Fix table edit and delete

--- src/resources/views/partials/table.blade.php
+++ src/resources/views/partials/table.blade.php
@@ -37,8 +37,8 @@
 											<td>{!! $value !!}</td>
 										@endforeach
 										<td>
-											{!! Form::open(['method' => 'DELETE', 'url' => Request::url() . '/' . $item->values()->first()]) !!}
-											@if ($edit) <a href="{{ Request::url() }}/{{ $item->values()->first() }}/edit" class="btn btn-xs blue">{{ trans('mconsole::tables.edit') }}</a> @endif
+											{!! Form::open(['method' => 'DELETE', 'url' => Request::url() . '/' . $item->get('#')]) !!}
+											@if ($edit) <a href="{{ Request::url() }}/{{ $item->get('#') }}/edit" class="btn btn-xs blue">{{ trans('mconsole::tables.edit') }}</a> @endif
 											@if ($delete) {!! Form::submit(trans('mconsole::tables.delete'), ['class' => 'btn btn-xs btn-danger delete-confirm']) !!} @endif
 											{!! Form::close() !!}
 										</td>
