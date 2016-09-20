827efd20f90f2ff5180f875c2e3a129ab7bba8d8
Eugene Min <e.min@milax.com>
2016-09-20 14:09:11 +1100

    Ability to set text and select null labels

--- src/resources/views/forms/select.blade.php
+++ src/resources/views/forms/select.blade.php
@@ -1,5 +1,5 @@
 <div class="form-group">
-	<label>{{ $label }}</label>
+	@if (!empty($label))<label>{{ $label }}</label>@endif
     <?php if (isset($value)): $val = $value; elseif (is_bool(Form::getValueAttribute($name))): $val = (int) Form::getValueAttribute($name); else: $val = null; endif; ?>
     @if (isset($options) && is_array($options))
         {!! Form::select($name, $options, $val, ['class' => sprintf('form-control %s', isset($class) ? $class : null)]) !!}
