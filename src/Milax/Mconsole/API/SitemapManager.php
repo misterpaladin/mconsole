6bedab0f5a76bdf9b4c32a1e7ee4f9846821edad
De Sik Min <e.min@milax.com>
2017-04-01 12:28:49 +1100

    Add missing comments

--- src/Milax/Mconsole/API/SitemapManager.php
+++ src/Milax/Mconsole/API/SitemapManager.php
@@ -8,17 +8,37 @@ class SitemapManager implements GenericAPI
 {
     protected $stack = [];
     
+    /**
+     * Register links provider.
+     * 
+     * @access public
+     * @param Closure $handler
+     * @return $this
+     */
     public function register($handler)
     {
         array_push($this->stack, $handler);
         return $this;
     }
-
+    
+    /**
+     * Register sitemap handler.
+     * 
+     * @access public
+     * @param \Milax\Mconsole\Contracts\Components\SitemapHandler $handler
+     * @return void
+     */
     public function setHandler(\Milax\Mconsole\Contracts\Components\SitemapHandler $handler)
     {
         $this->handler = $handler;
     }
-
+    
+    /**
+     * Handle sitemap generation.
+     * 
+     * @access public
+     * @return \Illuminate\Http\Response
+     */
     public function handle()
     {
         return $this->handler->handle($this->stack);
