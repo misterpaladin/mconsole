234ca36bb11fa7fe42c1b493450b1a7754ed02c5
De Sik Min <e.min@milax.com>
2017-06-05 15:09:49 +1100

    Fix for php 7.1

--- src/Milax/Mconsole/Http/Uploads/UploadHandler.php
+++ src/Milax/Mconsole/Http/Uploads/UploadHandler.php
@@ -370,11 +370,9 @@ class UploadHandler
         $last = strtolower($val[strlen($val)-1]);
         switch ($last) {
             case 'g':
-                $val *= 1024;
             case 'm':
-                $val *= 1024;
             case 'k':
-                $val *= 1024;
+                $val = (int) $val * 1024;
         }
         return $this->fix_integer_overflow($val);
     }
