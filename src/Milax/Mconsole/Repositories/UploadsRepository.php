cc04fcc240f60b792bad0e142f61abc9d5047b33
De Sik Min <e.min@milax.com>
2017-06-05 17:11:37 +1100

    Sort uploads by id desc #119

--- src/Milax/Mconsole/Repositories/UploadsRepository.php
+++ src/Milax/Mconsole/Repositories/UploadsRepository.php
@@ -8,4 +8,9 @@ use Milax\Mconsole\Contracts\Repositories\UploadsRepository as Repository;
 class UploadsRepository extends EloquentRepository implements Repository
 {
     public $model = \Milax\Mconsole\Models\Upload::class;
+
+    public function index()
+    {
+        return $this->query()->orderBy('id', 'DESC');
+    }
 }
