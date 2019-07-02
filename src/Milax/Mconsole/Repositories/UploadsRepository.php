1a1c6033b2c92d7c71978ca50311764d8b644e8d
rudenko-v <rudenko@arkout.ru>
2019-07-02 12:28:17 +1100

    Uploads: get by id

--- src/Milax/Mconsole/Repositories/UploadsRepository.php
+++ src/Milax/Mconsole/Repositories/UploadsRepository.php
@@ -4,13 +4,28 @@ namespace Milax\Mconsole\Repositories;
 
 use Milax\Mconsole\Repositories\EloquentRepository;
 use Milax\Mconsole\Contracts\Repositories\UploadsRepository as Repository;
+use Milax\Mconsole\Traits\Repositories\TaggableRepository;
 
 class UploadsRepository extends EloquentRepository implements Repository
 {
+    use TaggableRepository;
+
     public $model = \Milax\Mconsole\Models\Upload::class;
 
     public function index()
     {
         return $this->query()->orderBy('id', 'DESC');
+    }
+
+    /**
+     * Get uploads by id
+     * 
+     * @param  integer || array $id [Uploads id]
+     * @return collection
+     */
+    public function getById($id)
+    {
+        if (gettype($id) != 'array') $id = [$id];
+        return $this->query()->whereIn('id', $id)->orderByRaw(sprintf('FIELD(id,%s)', implode(',', $id)))->get();
     }
 }
