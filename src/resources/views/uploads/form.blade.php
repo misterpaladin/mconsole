b2020e1c55333196091070dcf317bd97166b95dd
rudenko-v <rudenko@arkout.ru>
2020-11-07 20:40:34 +1100

    Update upload edit form

--- src/resources/views/uploads/form.blade.php
+++ src/resources/views/uploads/form.blade.php
@@ -44,6 +44,41 @@
         <div class="portlet light">
 			<div class="portlet-title">
 				<div class="caption">
+					<span class="caption-subject font-blue sbold uppercase">{{ trans('mconsole::uploads.table.copies') }}</span>
+				</div>
+			</div>
+			<div class="portlet-body form">
+                @if (isset($item))
+                    <div class="row">
+                    @foreach($item->getCopies(true) as $key => $copy)
+                        <div class="col-sm-6 col-md-4">
+                            <div class="thumbnail">
+                            <img src="{{ $copy }}" alt="">
+                            <div class="caption">
+                                @if(substr(mime_content_type('.'.$copy),0,5) === 'image')
+                                    @php
+                                        list($w, $h) = getimagesize('.'.$copy);
+                                    @endphp
+                                    <h4>{{ sprintf('%s (%sx%s)', $key, $w, $h) }}</h4>
+                                @else
+                                    <h4>{{ $key }}</h4>
+                                @endif
+                                <p><a href="{{ $copy }}" target="_blank" class="btn btn-primary" role="button"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></a> <a href="#" class="btn copy" data-clipboard-text="{{ $copy }}"><span class="glyphicon glyphicon-duplicate" aria-hidden="true"></span></a></p>
+                            </div>
+                            </div>
+                        </div>
+                        @if(!($loop->iteration%3) && $loop->iteration != $loop->last)</div><div class="row">@endif
+                    @endforeach
+                    </div>
+                @endif
+			</div>
+		</div>
+    </div>
+
+    <div class="col-lg-5 col-md-6">
+        <div class="portlet light">
+			<div class="portlet-title">
+				<div class="caption">
 					<span class="caption-subject font-blue sbold uppercase">{{ trans('mconsole::forms.tags.label') }}</span>
 				</div>
 			</div>
@@ -65,3 +100,9 @@
 </div>
 
 {!! Form::close() !!}
+
+@section('page.scripts')
+    <script>
+        new Clipboard('.copy');
+    </script>
+@endsection
