2db87a441bd9067c8173e56c074dd0b123bb65c3
rudenko-v <rudenko@arkout.ru>
2020-11-07 19:08:55 +1100

    Update exception handler for laravel 5.8

--- src/Milax/Mconsole/Exceptions/Handlers/MconsoleExceptionHandler.php
+++ src/Milax/Mconsole/Exceptions/Handlers/MconsoleExceptionHandler.php
@@ -9,8 +9,9 @@ use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Contracts\Debug\ExceptionHandler as ExceptionHandlerContract;
 
-class MconsoleExceptionHandler extends ExceptionHandler
+class MconsoleExceptionHandler extends ExceptionHandler implements ExceptionHandlerContract
 {
     /**
      * A list of the exception types that should not be reported.
@@ -40,6 +41,17 @@ class MconsoleExceptionHandler extends ExceptionHandler
     }
 
     /**
+     * Determine if the exception should be reported.
+     *
+     * @param  \Exception  $e
+     * @return bool
+     */
+    public function shouldReport(Exception $e)
+    {
+        parent::shouldReport($e);
+    }
+
+    /**
      * Render an exception into an HTTP response.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -48,34 +60,41 @@ class MconsoleExceptionHandler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
+        if ($e instanceof HttpException) {
+            $status = $e->getStatusCode();
+            $trans = trans('mconsole::mconsole.errors');
+            
+            if (Request::segment(1) == 'mconsole' && isset($trans[$status])) {
+                $view = 'mconsole::errors.error';
+            } else {
+                $view = sprintf('errors.%s', $status);
+            }
+            
+            if (view()->exists($view)) {
+                return response()->view($view, [
+                    'exception' => $e,
+                    'status' => $status,
+                    'text' => $trans,
+                ], $status, $e->getHeaders());
+            } else {
+                return $this->convertExceptionToResponse($e);
+            }
+        }
+
         return parent::render($request, $e);
     }
-    
+
     /**
-     * Render the given HttpException.
+     * Render an exception to the console.
      *
-     * @param  \Symfony\Component\HttpKernel\Exception\HttpException  $e
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @param  \Exception  $e
+     * @return void
      */
-    protected function renderHttpException(HttpException $e)
+    public function renderForConsole($output, Exception $e)
     {
-        $status = $e->getStatusCode();
-        $trans = trans('mconsole::mconsole.errors');
-        
-        if (Request::segment(1) == 'mconsole' && isset($trans[$status])) {
-            $view = 'mconsole::errors.error';
-        } else {
-            $view = sprintf('errors.%s', $status);
-        }
-        
-        if (view()->exists($view)) {
-            return response()->view($view, [
-                'exception' => $e,
-                'status' => $status,
-                'text' => $trans,
-            ], $status, $e->getHeaders());
-        } else {
-            return $this->convertExceptionToResponse($e);
-        }
+        parent::renderForConsole($output, $e);
     }
+    
+    
 }
