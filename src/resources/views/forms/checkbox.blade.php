59b8fea7d54f444084f22509d24bcb2ffcaea869
Vladimir Rudenko <rudenko@arkout.ru>
2024-08-26 16:45:03 +1100

    Update checkboxes views

--- src/resources/views/forms/checkbox.blade.php
+++ src/resources/views/forms/checkbox.blade.php
@@ -3,7 +3,7 @@
     <div class="checkbox-list">
         @php
             $isChecked = !is_null($value) ? ($value == true ? true : false)
-                : (!is_null($checked) && $checked == true ? true : false);
+                : (isset($checked) && !is_null($checked) && $checked == true ? true : false);
         @endphp
         <label class="checkbox"><input {{ $isChecked == true ? 'checked="checked"' : '' }} name="{{ $name }}" type="checkbox" value="1"> {{ $label }}</label>
     </div>
--- src/resources/views/forms/checkboxes.blade.php
+++ src/resources/views/forms/checkboxes.blade.php
@@ -5,9 +5,8 @@
     <div class="checkbox-list">
         @foreach ($items as $checkbox)
             @php
-                $isChecked = !is_null($checkbox['name'])
-                    ? (int) $checkbox['name']
-                    : (int) (isset($checkbox['checked']) && $checkbox['checked'] == true)
+                $isChecked = !is_null($checkbox['value']) ? ($checkbox['value'] == true ? true : false)
+                    : (isset($checkbox['checked']) && !is_null($checkbox['checked']) && $checkbox['checked'] == true ? true : false);
             @endphp
             <label class="checkbox-inline"><input {{ $isChecked != 1 ? '' : 'checked="checked"' }} name="{{ $checkbox['name'] }}" type="checkbox" value="1"> {{ $checkbox['label'] }}</label>
         @endforeach
