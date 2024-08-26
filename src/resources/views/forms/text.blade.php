0dd0accd4dc86a909c1e2e701443858a0407b27f
Vladimir Rudenko <rudenko@arkout.ru>
2024-08-26 16:45:16 +1100

    Update text field view

--- src/resources/views/forms/text.blade.php
+++ src/resources/views/forms/text.blade.php
@@ -1,5 +1,7 @@
 <div class="form-group">
-    <label>{!! $label !!} @if (isset($popover) && strlen($popover) > 0) <i class="fa fa-question-circle popovers" data-container="body" data-trigger="hover" data-placement="top" data-content="{{ trans($popover) }}"></i> @endif</label>
+    @if($label)
+        <label>{!! $label !!} @if (isset($popover) && strlen($popover) > 0) <i class="fa fa-question-circle popovers" data-container="body" data-trigger="hover" data-placement="top" data-content="{{ trans($popover) }}"></i> @endif</label>
+    @endif
 
     <input class="form-control {{ isset($class) ? $class : '' }}" type="text" autocomplete="off" placeholder="{{ isset($placeholder) ? $placeholder : null }}" name="{{ $name }}" value="{{ isset($value) ? $value : (is_null(old($name)) ? null : old($name)) }}">
 </div>
